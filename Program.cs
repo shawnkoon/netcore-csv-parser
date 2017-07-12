ae555eb5b5a8075714887d5ba670458bd924fa2b
Shawn Son <shawn9405@gmail.com>
2017-07-12 16:06:19 -0700

    Added basic project structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netcore_csv_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # netcore-csv-parser
 Project focused on Creating Dotnet Core project from scratch to fully working + fully unit tested product.
+
+
+# To Run
+`$ dotnet restore`
+`$ dotnet build`
+`$ dotnet run`
